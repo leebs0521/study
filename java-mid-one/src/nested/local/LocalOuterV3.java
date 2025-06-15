54bad2681e48dbbd95757eb90f0e81149234fa04
leebs0521 <leebs0521@gmail.com>
2025-06-15 21:51:13 +0900

    2025.06.15: java mid one - inner & nested class

---
+++ java-mid-one/src/nested/local/LocalOuterV3.java
@@ -0,0 +1,45 @@
+package nested.local;
+
+import java.lang.reflect.Field;
+
+public class LocalOuterV3 {
+
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar) {
+        int localVar = 1;
+
+        class LocalPrinter implements Printer {
+
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+
+                // 인스턴스는 지역 변수 보다 오래 살아 남는다.
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+
+        return new LocalPrinter(); // 인스턴스만 반환
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV3 localOuter = new LocalOuterV3();
+        Printer printer = localOuter.process(2);
+
+        //printer.print()를 나중에 실행한다. process()의 스택 프레임이 사라진 이후에 실행
+        printer.print();
+
+        //추가
+        System.out.println("필드 확인");
+        Field[] fields = printer.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field);
+        }
+    }
+}
