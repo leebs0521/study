37a543943e8f4411b3a4cbc5e7fb298b0d12f659
leebs0521 <leebs0521@gmail.com>
2025-06-28 22:18:14 +0900

    2025.06.28: java advance one - thread

---
+++ java-advance-one/src/thread/control/printer/MyPrinterV4.java
@@ -0,0 +1,57 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+
+public class MyPrinterV4 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread thread = new Thread(printer, "printer");
+        thread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            log("프린트할 문서를 입력하세요. 종료 (q): ");
+            String input = scanner.nextLine();
+            if (input.equals("q")) {
+                thread.interrupt();
+                break;
+            }
+
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                if (jobQueue.isEmpty()) {
+                    Thread.yield();
+                    continue;
+                }
+
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료: " + job);
+                } catch (InterruptedException e) {
+                    log("인터럽트 !");
+                    break;
+                }
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
