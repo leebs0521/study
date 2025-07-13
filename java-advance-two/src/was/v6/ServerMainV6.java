a23bb3850e8985952757fa49814c01de7d0ffbe6
leebs0521 <leebs0521@gmail.com>
2025-07-13 21:27:06 +0900

    25.07.13: java advance two - HTTP server with reflection

---
+++ java-advance-two/src/was/v6/ServerMainV6.java
@@ -0,0 +1,30 @@
+package was.v6;
+
+import was.httpserver.HttpServer;
+import was.httpserver.HttpServlet;
+import was.httpserver.ServletManager;
+import was.httpserver.servlet.DiscardServlet;
+import was.httpserver.servlet.ReflectionServlet;
+import was.v5.servlet.HomeServlet;
+import was.v5.servlet.SearchServlet;
+import was.v5.servlet.Site1Servlet;
+import was.v5.servlet.Site2Servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ServerMainV6 {
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        List<Object> controllers = List.of(new SiteControllerV6(), new SearchControllerV6());
+        HttpServlet reflectionServlet = new ReflectionServlet(controllers);
+
+        ServletManager servletManager = new ServletManager();
+        servletManager.setDefaultServlet(reflectionServlet);
+        servletManager.add("/", new HomeServlet());
+        servletManager.add("/favicon.ico", new DiscardServlet());
+        HttpServer server = new HttpServer(PORT, servletManager);
+        server.start();
+    }
+}
